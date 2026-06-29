1995241caf7c3eb45dd4acb3517da0982311f7ce
Anshul Ramdham <anshulramdham91@gmail.com>
2026-06-29 19:09:35 +0530

    Project Uploaded

---
+++ FinSight_AI_Smart_Expense_Analyzer/src/main/java/com/finsight/demo/FinSightAiSmartExpenseAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.finsight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSightAiSmartExpenseAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinSightAiSmartExpenseAnalyzerApplication.class, args);
+	}
+
+}

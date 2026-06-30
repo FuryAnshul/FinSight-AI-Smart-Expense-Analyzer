378782a9c98c8594760d5882f1f9336e9e51403f
TejasGathe <gathetejas2001@gmail.com>
2026-06-30 13:36:27 +0530

    commit 1

---
+++ FinSight_AI_Smart_Expense_Analyzer/src/main/java/com/finsight/demo/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.finsight.demo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finsight.demo.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
